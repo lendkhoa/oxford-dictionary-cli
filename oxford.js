4df1aad2e128fe6cc239f91f7f12cb42a1270443
KL052362 <khoa.le@cerner.com>
2017-10-18 13:16:34 -0500

    added antonyms and synonyms results

--- oxford.js
+++ oxford.js
@@ -1,13 +1,25 @@
 'use strict'
 /**
  * Quickly searches Oxford Dictionary
- * @author: Khoa Le
- * @version: Oct 17, 2017
+ * @version: Oct 18, 2017
  */
+
+const Dictionary = require("../node_modules/oxford-dictionary");
+
+const CONFIG = {
+  app_id : "YOUR_APP_ID",
+  app_key : "YOUR_APP_KEY",
+  source_lang : "en"
+};
+
+const FIRST_PARAM = 2;
+
+let dict = new Dictionary(CONFIG);
 
 /**
  * For future reference, this is the return from
- * Oxford API for the definitions command.
+ * Oxford API definition search
+ * Notice definition object != thesaurus object (structure wise)
  */
 let mockObj = {
   "metadata": {
@@ -49,20 +61,7 @@ let mockObj = {
   ]
 };
 
-const Dictionary = require("oxford-dictionary");
-
-const CONFIG = {
-  app_id : "YOUR_ACCT_APP_ID",
-  app_key : "YOUR_APP_KEY",
-  source_lang : "en"
-};
-
-let dict = new Dictionary(CONFIG);
-
-process.argv.forEach((val, index) => {
-  if (index === 2) {
-    let lookup = dict.definitions(val);
-
+const definitionHandler = (lookup) =>{
     lookup.then(function(res){
           // res = mockObj
           for (let theRes = 0; theRes < res.results.length; theRes++){
@@ -84,7 +83,52 @@ process.argv.forEach((val, index) => {
           }
     },
     function(err) {
-        console.log(err);
+        console.log("No definition found");
     });
+}
+
+const thesaurusHandler = (thesaurus)=>{
+    thesaurus.then((res)=>{
+        for (let theRes = 0; theRes < res.results.length; theRes++){
+            const lexicalObj = res.results[theRes].lexicalEntries;
+            for (let lexEn = 0; lexEn < lexicalObj.length; lexEn++) {
+                const lexEntries = lexicalObj[lexEn];
+                for (let entry = 0; entry < lexEntries.entries.length; entry++) {
+                    const sensesObj = lexEntries.entries[entry];
+                    let antonymsLst = "";
+                    let synonymsLst = "";
+                    for (let i = 0; i < sensesObj.senses.length; i++) {
+                        const sense = sensesObj.senses[i];
+                        let synonyms = sense.synonyms;
+                        let antonyms = sense.antonyms;
+                        // traverse synonyms and antonyms
+                        if (synonyms) {
+                            synonyms.forEach((e)=>{
+                                synonymsLst += `${e.text}, `;
+                            })
+                        }
+                        if (antonyms) {
+                            antonyms.forEach((e)=>{
+                                antonymsLst += `${e.text}, `;
+                            })
+                        }
+                    }
+                    console.log(`\n`);
+                    console.log(`> antonyms: ${antonymsLst}`);
+                    console.log(`> synonyms: ${synonymsLst}`);
+                }
+            }
+        }
+    }, (err)=>{
+        console.log("No thesaurus found");
+    });
+}
+
+process.argv.forEach((val, index) => {
+  if (index === FIRST_PARAM) {
+    const lookup = dict.definitions(val);
+    const thesaurus = dict.thesaurus(val);
+    definitionHandler(lookup);
+    thesaurusHandler(thesaurus);
   }
 });
